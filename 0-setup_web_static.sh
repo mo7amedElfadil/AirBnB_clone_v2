b4dcaf6d846f27f455509a4b44a4c43d3a95d95c
mo7amedelfadil <mohamedelfadil1994@gmail.com>
2024-03-06 20:42:36 +0200

    task 0 preparing our web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -65,18 +65,18 @@ my_setup="server {
 	location / {
 		try_files \$uri \$uri/ =404;
 	}
-	location /redirect_me {
-		return 301 https://www.youtube.com/watch?v=dQw4w9WgXcQ;
-	}
 
 	location /hbnb_static/ {
 		alias /data/web_static/current/;
-		index index.html;
+	}
+	location /redirect_me {
+		return 301 https://www.youtube.com/watch?v=dQw4w9WgXcQ;
 	}
 	error_page 404 /404.html;
 	location = /404.html {
 		internal;
 	}
+
 }" 
 	echo -e "$my_setup" | sudo tee /etc/nginx/sites-available/default > /dev/null
 
