012bac8d6e661deca03c20eb0c8c0fc3bd996b3f
mo7amedelfadil <mohamedelfadil1994@gmail.com>
2024-03-07 02:05:57 +0200

    forward slash

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -44,7 +44,7 @@ def do_deploy(archive_path):
 
         run(f"rm /tmp/{archive_path}")
         run("rm -rf /data/web_static/current")
-        run(f"ln -s {target}{file}/ /data/web_static/current")
+        run(f"ln -s {target}{file} /data/web_static/current")
 
         print("New version deployed!")
 
