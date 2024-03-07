02ff25488b227ab96d11c43a55f04715cfbd62aa
mo7amedelfadil <mohamedelfadil1994@gmail.com>
2024-03-08 01:19:51 +0200

    return exec

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,19 +1,22 @@
 # This is a manifest file for the web_static deployment
 # It will install nginx, create the necessary directories and files, and configure the server to serve the content
-
+exec {'update':
+  command => '/usr/bin/apt-get update',
+}
 # install and configure nginx server
-package { 'nginx':
+-> package { 'nginx':
   ensure   => 'installed',
   provider => 'apt',
 }
 # create the necessary directories
-$dirs = ['/data', '/data/web_static', '/data/web_static/releases', '/data/web_static/shared', '/data/web_static/releases/test']
+$dir_names = ['/data', '/data/web_static', '/data/web_static/releases', '/data/web_static/shared', '/data/web_static/releases/test']
 
--> file { $dirs:
-  ensure => 'directory',
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
-  mode   => '0755',
+-> file { $dir_names:
+  ensure  => 'directory',
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  mode    => '0755',
+  require => Package['nginx'],
 }
 
 # create the necessary files
