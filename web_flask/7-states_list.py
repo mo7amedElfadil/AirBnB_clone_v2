621e86e92d159bb0fad017f90b1fb7dd0a926371
mo7amedelfadil <mohamedelfadil1994@gmail.com>
2024-03-23 03:34:22 +0200

    added old methods

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,6 +9,64 @@ from models.state import State
 app = Flask(__name__)
 
 
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Hello HBNB!
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    HBNB
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """
+    display C followed by text and replace '_' with space
+    """
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text):
+    """
+    display Python followed by text and replace '_' with space
+    """
+    return f"Python {text.replace('_', ' ')}"
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """
+    display n is a number only if n is an integer
+    """
+    return f"{n} is a number"
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """
+    display a HTML page only if n is an integer
+    """
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    """
+    display a HTML page only if n is an integer
+    """
+    text = ("even", "odd")[n % 2]
+    return render_template('6-number_odd_or_even.html', n=n, text=text)
+
+
 def get_storage():
     """
     Get storage instance
@@ -19,7 +77,7 @@ def get_storage():
 
 
 @app.teardown_appcontext
-def close_storage(exception):
+def close_storage(_):
     """
     Close storage
     """
