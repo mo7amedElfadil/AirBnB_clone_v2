fe3b7a3fabc911c938951a4c7173d19cc145f0fd
mo7amedelfadil <mohamedelfadil1994@gmail.com>
2024-03-23 04:37:04 +0200

    i will come back for the extra newlines

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -26,5 +26,14 @@ def states_list():
     return render_template('7-states_list.html', states=states)
 
 
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """
+    List all cities by states
+    """
+    states = storage.all(State)
+    return render_template('8-cities_by_states.html', states=states)
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
